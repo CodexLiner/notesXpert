a04f19248ebbb506b6031130870f5609adb04b85
Gopal Meena <meenagopal24@gmail.com>
2025-09-02 21:37:05 +0530

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,33 @@
+rootProject.name = "NotesXpert"
+
+pluginManagement {
+    repositories {
+        google {
+            content { 
+              	includeGroupByRegex("com\\.android.*")
+              	includeGroupByRegex("com\\.google.*")
+              	includeGroupByRegex("androidx.*")
+              	includeGroupByRegex("android.*")
+            }
+        }
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            content { 
+              	includeGroupByRegex("com\\.android.*")
+              	includeGroupByRegex("com\\.google.*")
+              	includeGroupByRegex("androidx.*")
+              	includeGroupByRegex("android.*")
+            }
+        }
+        mavenCentral()
+    }
+}
+include(":core")
+include(":app:composeApp")
+
